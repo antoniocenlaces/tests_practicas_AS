79148954944e837687c212ade54258ce186976b9
antoniocenlaces <antoniocenlaces@gmail.com>
2023-03-09 14:34:01 +0000

    Inicio script practica3

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 #779035, Bernad Ferrando, Lizer, T, 2, A
 #143045, Gonzalez Almela, Antonio, T, 2, A
+if [ $EUID -ne 0 ]
+then
+    echo "Este script necesita privilegios de administracion"
+    exit 1
+else
+    if [ $# -ne 2 ]
+    then
+        echo "Numero incorrecto de parametros"
+    else
+        case $1 in
+            -a)
+                echo "Opción -a leida";;
+            -s)
+                echo "Opción -s leida";;
+            *)
+                echo "Opción no válida";;
+        esac
+    fi
+fi
