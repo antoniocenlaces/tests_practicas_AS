19637d285b6c2f8bef83b00c5f47dd87a3dc4f33
antoniocenlaces <antoniocenlaces@gmail.com>
2023-03-28 15:00:20 +0000

    Acabada P3 como enivada a Lizer

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -10,36 +10,55 @@ else
     then
         echo "Numero incorrecto de parametros"
     else
+        # Cambia el separador por defecto a ','
         OLDIFS=$IFS
         IFS=','
         case "$1" in
             -a)
-                echo "Opción -a leida";;
-            -s)
-                # echo "Opción -s leida"
                 while read user pass nombre
                 do
-                    echo "Usuario: $user $pass $nombre está en el fichero";
+                    if [ -z "$user" || -z "$pass" || -z "$nombre" ] # Comprueba si algún campo leido del fichero está vacío
+                    then # Si algún campo no tiene valor muestra mensaje
+                        echo "Campo invalido"
+                        exit 1
+                    else
+                        id -u "$user" >/dev/null 2>/dev/null # Comprueba si existe este $user
+                        if [ $? -eq 0 ]
+                        then # El $user existe. Muestra mensaje
+                            echo "El usuario $user ya existe"
+                        else
+                            useradd "$user" -U -m -k /etc/skel -K UID_MIN=1815 -c "$nombre"
+                            usermod --expiredate "$(date -d "+30 days" +%Y-%m-%d)" "$user"
+                            echo "$user:$pass" | chpasswd
+                            echo "$nombre ha sido creado"
+                        fi
+                    fi
+                done < "$2";;
+            -s)
+               # echo "Opción -s leida"
+                if [ ! -d /extra/backup ] # Comprueba si ya existe el directorio para backup
+                then # Caso contrario lo crea
+                    mkdir -p /extra/backup
+                fi
+                while read user pass nombre
+                do
+                    # echo "Usuario: $user $pass $nombre está en el fichero";
                     id -u "$user" >/dev/null 2>/dev/null # Comprueba si existe este $user
                     if [ $? -eq 0 ]
                     then # El $user existe y podemos borrarlo
-                        if [ ! -d /extra/backup ] # Comprueba si ya existe el directorio para backup
-                        then # Caso contrario lo crea
-                            mkdir -p /extra/backup
-                        fi
-                        if [ -d /home/"$user" ] # Comprueba que este usuario tiene directorio local
+                        directorio=$(grep "$user" /etc/passwd | cut -d ':' -f 6)
+                        if [ -d "$directorio" ] # Comprueba que este usuario tiene directorio local
                         then
-                            tar -cf /extra/backup/"$nombre".tar /home/"$nombre"/ # Hace copia de seguridad de directorio local
+                            tar -cf "/extra/backup/$user".tar "$directorio/" # Hace copia de seguridad de directorio local
                         fi
-                        # Borra el usuario y su directorio /home/user
+                        # Borra el usuario y su directorio home
                         userdel -r "$user" >/dev/null 2>/dev/null
-                    else
-                        echo "El usuario $user no existe"
                     fi
                 done < "$2";;
             *)
                 echo "Opcion invalida" 2>/dev/stderr;;
         esac
+        # Restaura el separador por defecto
         IFS=$OLDIFS
     fi
 fi
