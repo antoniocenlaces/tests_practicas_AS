b44e16ce59c59d9e5d00e75629f01b84885e37c6
antoniocenlaces <antoniocenlaces@gmail.com>
2023-04-15 19:10:12 +0200

    Versión Lizer Práctica 4 en dir

---
+++ practica_4/779035_143045/practica_3.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#779035, Bernad Ferrando, Lizer, T, 2, A
+#143045, Gonzalez Almela, Antonio, T, 2, A
+if [ $EUID -ne 0 ]
+then
+	echo "Este script necesita privilegios de administracion"
+	exit 1
+else
+	if [ $# -ne 2 ]
+	then
+		echo "Numero incorrecto de parametros"
+	else
+		case "$1" in
+			-a)
+				while read linea
+				do
+					nombre=$(echo "$linea" | cut -d ',' -f 1)
+					clave=$(echo "$linea" | cut -d ',' -f 2)
+					apodo=$(echo "$linea" | cut -d ',' -f 3)
+					if [ -z "$nombre" ] || [ -z "$clave" ] || [ -z "$apodo" ]
+					then
+						echo "Campo invalido"
+						exit 1
+
+					fi
+					uid=$(id -u "$nombre" 2>/dev/null)
+					if [ -n "$uid" ]
+					then
+						echo "El usuario "$nombre" ya existe"
+					else
+						useradd "$nombre" -U -m -k /etc/skel -K UID_MIN=1815 -c "$apodo"
+						usermod --expiredate "$(date -d "+30 days" +%Y-%m-%d)" "$nombre"
+						echo "$nombre:$clave" | chpasswd
+						echo ""$apodo" ha sido creado"
+					fi
+				done < $2;;
+			-s)
+				if [ ! -d "/extra/backup" ]
+				then
+					if [ ! -d "/extra" ]
+					then
+						mkdir /extra
+						mkdir /extra/backup
+					else
+						mkdir /extra/backup
+					fi
+				fi
+				while read linea
+				do
+					nombre=$(echo "$linea" | cut -d ',' -f 1)
+					directorio=$(grep "$nombre" /etc/passwd | cut -d ':' -f 6)
+					homeUsuario="${directorio%/*}"
+					if tar -cf "$nombre".tar "$homeUsuario"/"$nombre"
+					then
+						mv "$nombre".tar /extra/backup
+						userdel -r "$nombre" 2>/dev/null
+					fi
+				done < $2;;
+			*)
+				echo "Opcion invalida" 2>/dev/stderr;;
+		esac
+	fi	
+fi
